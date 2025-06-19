5498ff4f40dec0ecaeb6a0b225d0376e6255736e
hachnv8 <hachnv8@gmail.com>
2025-06-20 00:40:24 +0700

    feat: Initial commit for infra setup

---
+++ infra-setup/scripts/start-infra.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NETWORK_NAME="infra-net"
+
+# Kiểm tra network đã tồn tại chưa
+if ! docker network ls --format '{{.Name}}' | grep -wq "$NETWORK_NAME"; then
+  echo "Network '$NETWORK_NAME' chưa tồn tại. Đang tạo mới..."
+  docker network create "$NETWORK_NAME"
+  echo "Tạo network '$NETWORK_NAME' thành công."
+else
+  echo "Network '$NETWORK_NAME' đã tồn tại, bỏ qua tạo mới."
+fi
+
+# Chạy docker-compose
+echo "Starting database services (MySQL, Redis)..."
+docker-compose -f docker-compose-database.yaml up -d
+
+echo "Starting Jenkins..."
+docker-compose -f docker-compose-jenkins.yaml up -d
+
+echo "Starting Kafka and Zookeeper..."
+docker-compose -f docker-compose-kafka.yaml up -d
+
+echo "All infrastructure services are starting up. Please allow some time for them to be fully operational."
+echo "You can check their status using 'docker ps'."
